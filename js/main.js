8329ab26d2d4b2788301de57b3ae19132b1ac4c3
QuiteCustard <56020602+QuiteCustard@users.noreply.github.com>
2023-01-23 16:00:01 +0000

    Size logo on scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 const toggle = document.querySelector(".menu-toggle");
+const sections = document.querySelectorAll("section:not(#hero)");
+const logoWrapper = document.querySelector(".img-wrapper");
 
 toggle.addEventListener("click", () => toggle.classList.toggle("open"));
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) logoWrapper.classList.add("intersecting");
+        else logoWrapper.classList.remove("intersecting");
+    })
+}, {
+    rootMargin: '0px 0px -75% 0px'
+})
+
+sections.forEach(section => observer.observe(section));
